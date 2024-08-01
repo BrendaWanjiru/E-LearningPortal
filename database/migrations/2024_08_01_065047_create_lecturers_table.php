04fc2bd47e583f1826718fd207b54c4ac648cb7f
BrendaWanjiru <brendawanjiru5@gmail.com>
2024-08-01 10:03:56 +0300

    updated providers, services and tables

---
+++ database/migrations/2024_08_01_065047_create_lecturers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLecturersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lecturers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('fullname');
+            $table->text('address');
+            $table->string('mobileno')->unique();
+            $table->date('dob');
+            $table->string('img');
+            $table->string('department');
+            $table->text('description');
+            $table->string('gender');
+            $table->string('country');
+            $table->string('state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lecturers');
+    }
+}
